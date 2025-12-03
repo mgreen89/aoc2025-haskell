3ae5a92813d6c44399275bbeca295ee28637ebe8
Matthew Green <matgreen@cisco.com>
2025-12-03 10:09:58 +0000

    Day 3a

--- src/AoC/Challenge.hs
+++ src/AoC/Challenge.hs
@@ -68,7 +68,7 @@ solutionList =
   , (mkDay_ 1, (Part2, SomeSolution day01b))
   , (mkDay_ 2, (Part1, SomeSolution day02a))
   , (mkDay_ 2, (Part2, SomeSolution day02b))
-  -- , (mkDay_ 3, (Part1, SomeSolution day03a))
+  , (mkDay_ 3, (Part1, SomeSolution day03a))
   -- , (mkDay_ 3, (Part2, SomeSolution day03b))
   -- , (mkDay_ 4, (Part1, SomeSolution day04a))
   -- , (mkDay_ 4, (Part2, SomeSolution day04b))
--- src/AoC/Challenge/Day03.hs
+++ src/AoC/Challenge/Day03.hs
@@ -4,17 +4,30 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day03 (
-
+  day03a,
 )
 where
 
--- day03a
 -- , day03b
 
 import AoC.Solution
+import Data.Char (digitToInt)
+import Data.List (tails)
 
-day03a :: Solution _ _
-day03a = Solution{sParse = Right, sShow = show, sSolve = Right}
+day03a :: Solution [[Int]] Int
+day03a =
+  Solution
+    { sParse = Right . fmap (fmap digitToInt) . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap go
+    }
+ where
+  go xs =
+    maximum
+      [ x * 10 + y
+      | x : ys <- tails xs
+      , y <- ys
+      ]
 
 day03b :: Solution _ _
 day03b = Solution{sParse = Right, sShow = show, sSolve = Right}
