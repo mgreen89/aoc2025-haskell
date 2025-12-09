96050ba3950a1f6d268ef74f500ad5a220f15423
Matthew Green <matgreen@cisco.com>
2025-12-09 20:33:13 +0000

    Day 9b

--- src/AoC/Challenge.hs
+++ src/AoC/Challenge.hs
@@ -81,7 +81,7 @@ solutionList =
   , (mkDay_ 8, (Part1, SomeSolution day08a))
   , (mkDay_ 8, (Part2, SomeSolution day08b))
   , (mkDay_ 9, (Part1, SomeSolution day09a))
-  -- , (mkDay_ 9, (Part2, SomeSolution day09b))
+  , (mkDay_ 9, (Part2, SomeSolution day09b))
   -- , (mkDay_ 10, (Part1, SomeSolution day10a))
   -- , (mkDay_ 10, (Part2, SomeSolution day10b))
   -- , (mkDay_ 11, (Part1, SomeSolution day11a))
--- src/AoC/Challenge/Day09.hs
+++ src/AoC/Challenge/Day09.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day09 (
   day09a,
+  day09b,
 )
 where
-
--- , day09b
 
 import AoC.Solution
 import AoC.Util (maybeToEither)
@@ -17,11 +11,10 @@ import Data.List (sortOn, tails)
 import Data.Ord (Down (..))
 import Data.Void (Void)
 import Linear (V2 (..))
-import qualified Linear as L
+import Safe (headMay)
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import qualified Text.Megaparsec.Char.Lexer as MPL
-import Safe (headMay)
 
 parser :: MP.Parsec Void String [V2 Int]
 parser = flip MP.sepBy (MP.char '\n') $ do
@@ -36,17 +29,61 @@ parse =
 sqA :: V2 Int -> V2 Int -> Int
 sqA (V2 x1 y1) (V2 x2 y2) = (abs (x2 - x1) + 1) * (abs (y2 - y1) + 1)
 
+allPairs :: [a] -> [(a, a)]
+allPairs xs = [(a, b) | a : as <- tails xs, b <- as]
+
+consecPairs :: [a] -> [(a, a)]
+consecPairs xs = zip xs (tail xs) ++ [(last xs, head xs)]
+
 day09a :: Solution [V2 Int] Int
 day09a =
   Solution
     { sParse = parse
     , sShow = show
     , sSolve =
-        maybeToEither "no head"
+        maybeToEither "no areas"
           . headMay
           . sortOn Down
-          . (\ts -> [sqA p q | p : ps <- tails ts, q <- ps])
+          . fmap (uncurry sqA)
+          . allPairs
     }
 
-day09b :: Solution _ _
-day09b = Solution{sParse = Right, sShow = show, sSolve = Right}
+-- Check if an axis-aligned segment intersects with an axis-aligned rectangle.
+--  N.B. touching the rectangle boundary doesn't count.
+-- First: rectangle
+-- Second: segment
+boxIntersect :: (V2 Int, V2 Int) -> (V2 Int, V2 Int) -> Bool
+boxIntersect (V2 a b, V2 c d) (V2 p q, V2 r s) =
+  let
+    onLeft = p <= min a c && r <= min a c
+    onRight = p >= max a c && r >= max a c
+    above = q >= max b d && s >= max b d
+    below = q <= min b d && s <= min b d
+   in
+    not (onLeft || onRight || above || below)
+
+solveB :: [V2 Int] -> Either String Int
+solveB pts =
+  maybeToEither "no areas"
+    . fmap snd
+    . headMay
+    . filter (noneIntersecting . fst)
+    . sortOn (Down . snd)
+    . fmap (\p -> (p, uncurry sqA p))
+    . allPairs
+    $ pts
+ where
+  edges :: [(V2 Int, V2 Int)]
+  edges = consecPairs pts
+
+  noneIntersecting :: (V2 Int, V2 Int) -> Bool
+  noneIntersecting box =
+    not . any (boxIntersect box) $ edges
+
+day09b :: Solution [V2 Int] Int
+day09b =
+  Solution
+    { sParse = parse
+    , sShow = show
+    , sSolve = solveB
+    }
