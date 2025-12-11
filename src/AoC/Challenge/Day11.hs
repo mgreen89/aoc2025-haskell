93c046d5906a440c00ca6152eb16fc240ef27e3c
Matthew Green <matgreen@cisco.com>
2025-12-11 09:29:34 +0000

    Day 11a

--- src/AoC/Challenge/Day11.hs
+++ src/AoC/Challenge/Day11.hs
@@ -4,17 +4,43 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day11 (
-
+  day11a
 )
 where
 
--- day11a
+
 -- , day11b
 
-import AoC.Solution
 
-day11a :: Solution _ _
-day11a = Solution{sParse = Right, sShow = show, sSolve = Right}
+import AoC.Solution
+import AoC.Util (maybeToEither)
+import Data.List (init, uncons)
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Debug.Trace
+
+
+-- Parse input like:
+-- hhh: ccc fff iii
+parse :: String -> Either String [(String, [String])]
+parse =
+  maybeToEither "device with no output"
+  . traverse (fmap (\(x, xs) -> (init x, xs)) . uncons . words)
+  . lines
+
+solveA :: [(String, [String])] -> Either String Int
+solveA devices = maybeToEither "no 'you' entry" $ M.lookup "you" pcm
+  where
+    pcm :: Map String Int
+    pcm = fmap (sum . fmap countPaths) $ M.fromList devices
+
+    countPaths :: String -> Int
+    countPaths "out" = 1
+    countPaths s = pcm M.! s
+
+day11a :: Solution [(String, [String])] Int
+day11a = Solution{sParse = parse, sShow = show, sSolve = solveA}
 
 day11b :: Solution _ _
 day11b = Solution{sParse = Right, sShow = show, sSolve = Right}
